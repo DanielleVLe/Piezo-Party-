db2a204c9d846e5e180d89039ee2bcf29bde2054
Danielle Le <danielle.le04@gmail.com>
2018-09-21 09:50:40 -0400

    Piezo Speakers
    Piezo plays different tones depending on what switches are pressed.

---
+++ sketch_sep21a/sketch_sep21a.ino
@@ -0,0 +1,16 @@
+int greenSwitch = 8;
+int redSwitch = 7;
+int whiteSwitch = 6;
+int piezoPin = 5; 
+int toneA = tone(piezoPin);
+
+void setup() {
+pinMode (greenSwitch, INPUT);
+pinMode (redSwitch,INPUT);
+pinMode (whiteSwitch, INPUT);
+
+void loop () {
+
+  
+}
+
